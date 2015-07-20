54d654ea68d53fb1fa925c4be965ee8fe8dcfa61
JORGE POLANCO <DRJORGEPOLANCO@GMAIL.com>
2015-07-19 20:08:04 -0400

    Add code to inherit __destruct method from parent Class

--- php_oop/inheritance.php
+++ php_oop/inheritance.php
@@ -6,12 +6,18 @@
         public function increaseEvilness() {
             ++$this -> evilness;
         }
+
+        public function __destruct() {
+            echo "You beat the Baddie!";
+        }
     }
 
     class Boss extends Baddie {
         public $evilness = 50;
 
         public function __destruct() {
+            parent::__destruct();
+            
             echo "You beat the Boss!";
         }
     }
